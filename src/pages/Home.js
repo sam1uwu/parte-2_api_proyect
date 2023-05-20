e8f4425434898730ab970eef4f9b5d567d0fce09
sam1uwu <sz84game@gmail.com>
2023-05-20 12:49:05 -0600

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import Axios from "../services/conexionAxios"
+
+export default function Home() {
+
+    const [empleado, setEmpleado] = useState([]);
+
+    const buscarEmpleado = async ()=> {
+    const buscarEmp= await Axios.get("/empleado/consultarEmpleados/")
+    setEmpleado(buscarEmp.data);
+
+    }
+    useEffect(()=>{
+        buscarEmpleado();
+    },[])
+
+return(
+    <div>
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title {empleado.nombre}</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+   )
+} 
+
